05332393bbe70b8c6534bebd6afb4214c67feec8
Muhammad Ahsan Farooq <ahsan.farooq@rolustech.net>
2022-09-26 18:40:57 +0500

    about us complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import './App.css';
+import About from './components/about';
 import NavBar from './components/navBar';
 import TextForm from './components/TextForm';
-
+import BasicForm from './components/forms/basicForm'
 function App()
 {
   return (
     <div className="App">
+    {/* <BasicForm/> */}
     <NavBar title="TextUtils" about="About TextUtils"/>
     <div className="container my-3">
-      <TextForm heading="Enter the text to analyze below"/>
+      {/* <TextForm heading="Enter the text to analyze below"/> */}
+      <About/>
     </div>
     </div>
   );
